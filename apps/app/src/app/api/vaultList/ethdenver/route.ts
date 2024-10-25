a30859eec1926c2fd6d29f4810297a3f9f8f3e7a
My EVM <yo@myevm.org>
2024-10-25 13:55:26 +0000

    .

--- apps/app/src/app/api/vaultList/ethdenver/route.ts
+++ apps/app/src/app/api/vaultList/ethdenver/route.ts
@@ -1,5 +1,4 @@
 import ethdenverVaultList from '@vaultLists/ethdenver'
-import memeVaultList from '@vaultLists/meme'
 import { NextResponse } from 'next/server'
 
 export function GET(): NextResponse {
--- apps/app/src/app/api/vaultList/route.ts
+++ apps/app/src/app/api/vaultList/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
 
 export function GET(): NextResponse {
-  return NextResponse.json({ vaultLists: ['default', 'meme'] }, { status: 200 })
+  return NextResponse.json({ vaultLists: ['default', 'meme','desci','rwa','rehab','refi','projects','opensource','nsfw','nfts','ethdenver','creators','causes','artists'] }, { status: 200 })
 }
