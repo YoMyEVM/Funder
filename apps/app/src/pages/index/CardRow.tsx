d28136048d3b4d8edf694e3b143d25a4b307eb50
My EVM <yo@myevm.org>
2024-10-25 08:05:23 +0000

    .

--- apps/app/src/pages/index/CardRow.tsx
+++ apps/app/src/pages/index/CardRow.tsx
@@ -3,15 +3,12 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const gooes = [
-  ['#FDEB71', '#F8D800'],
-  ['#ABDCFF', '#0396FF'],
-  ['#FEB692', '#EA5455'],
-  ['#CE9FFC', '#7367F0'],
-  ['#90F7EC', '#32CCBC'],
-  ['#FFF6B7', '#F6416C'],
-  ['#81FBB8', '#28C76F'],
-  ['#E2B0FF', '#9F44D3'],
-
+  { start: '#81FBB8', end: '#28C76F', title: 'Top Inflows' },
+  { start: '#FFF6B7', end: '#F6416C', title: 'Top Outflows' },
+  { start: '#FEB692', end: '#EA5455', title: 'Most Active' },
+  { start: '#CE9FFC', end: '#7367F0', title: 'Top Voted' },
+  { start: '#90F7EC', end: '#32CCBC', title: 'Best Performer' },
+  { start: '#E2B0FF', end: '#9F44D3', title: 'Recent Deposited' },
 ];
 
 const CardRow: React.FC = () => {
@@ -23,14 +20,16 @@ const CardRow: React.FC = () => {
 
   return (
     <PaletteContainer>
-      {gooes.map(([start, end], index) => (
+      {gooes.map(({ start, end, title }, index) => (
         <ColorCard
           key={`${start}-${end}`}
           start={start}
           end={end}
           isSelected={index === selectedIndex}
           onClick={() => handleSelect(index)}
-        />
+        >
+          <CardTitle>{title}</CardTitle>  {/* Display the title */}
+        </ColorCard>
       ))}
     </PaletteContainer>
   );
@@ -54,15 +53,25 @@ const PaletteContainer = styled.div`
 
 const ColorCard = styled.div<ColorCardProps>`
   flex: 1 1 8%;
-  height: 100px;
+  height: 130px;
   margin: 5px;
   background-image: linear-gradient(135deg, ${({ start }) => start} 0%, ${({ end }) => end} 100%);
   border-radius: 8px;
   cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: black;
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.7);
   transition: transform 0.2s ease;
   outline: ${({ isSelected, start }) => (isSelected ? `2px solid ${start}` : 'none')};
 
   &:hover {
     transform: scale(1.05);
   }
+`;
+
+const CardTitle = styled.div`
+  font-size: 18px;
+  text-align: center;
 `;
