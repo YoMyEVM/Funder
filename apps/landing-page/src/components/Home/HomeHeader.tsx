0f329a598e2ee62daaf6f6f4caf6eb754a17e920
My EVM <yo@myevm.org>
2024-10-27 07:10:07 +0000

    .

--- apps/landing-page/src/components/Home/HomeHeader.tsx
+++ apps/landing-page/src/components/Home/HomeHeader.tsx
@@ -15,7 +15,7 @@ export const HomeHeader = (props: HomeHeaderProps) => {
         <FlippingText />
 
         <div className="flex flex-col items-center text-center mb-10 z-10 md:mt-6 md:mb-10">
-          <h3 className="md:text-5xl text-pt-teal">
+          <h3 className="md:text-4xl text-pt-teal">
             Leveraging the <span className="text-pt-purple-200">Power</span> <span className="text-pt-purple-200">of</span> <span className="text-pt-purple-200">Web3</span> and <span className="text-pt-purple-200"> Community</span> to Fund the Impossible
           </h3>
         </div>
