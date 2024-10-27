1144ed45e60e2bf9f16d3d7355eff4fd4c067ff3
My EVM <yo@myevm.org>
2024-10-27 06:33:06 +0000

    .

--- apps/app/src/components/Vault/ButtonRow.tsx
+++ apps/app/src/components/Vault/ButtonRow.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import styles from './ButtonRow.module.css';
 
-const ButtonRow = () => {
+interface ButtonRowProps {
+  setSelectedButton: React.Dispatch<React.SetStateAction<string | null>>;
+}
+
+const ButtonRow: React.FC<ButtonRowProps> = ({ setSelectedButton }) => {
   return (
     <div className={styles.wrap}>
-      <a href="#" className={`${styles.button} ${styles.blue}`}>Yield Lotto</a>
-      <a href="#" className={styles.button}>Open Source</a>
-      <a href="#" className={`${styles.button} ${styles.green}`}>Projects</a>
-      <a href="#" className={`${styles.button} ${styles.purple}`}>Research</a>
-      <a href="#" className={`${styles.button} ${styles.yellow}`}>NFTs</a>
-      <a href="#" className={`${styles.button} ${styles.blue}`}>Culture/Meme</a>
-      <a href="#" className={styles.button}>Good Causes</a>
-      <a href="#" className={`${styles.button} ${styles.green}`}>Creators</a>
-      <a href="#" className={`${styles.button} ${styles.purple}`}>Artists</a>
-      <a href="#" className={`${styles.button} ${styles.yellow}`}>NSFW</a>
-      <a href="#" className={`${styles.button} ${styles.blue}`}>Longtail</a>
-      <a href="#" className={styles.button}>Eth Denver</a>
-      <a href="#" className={`${styles.button} ${styles.green}`}>DeScience</a>
-      <a href="#" className={`${styles.button} ${styles.purple}`}>ReFinance</a>
-      <a href="#" className={`${styles.button} ${styles.yellow}`}>Self Growth</a>
+      <a href="#" className={`${styles.button} ${styles.blue}`} onClick={() => setSelectedButton('Yield Lotto')}>Yield Lotto</a>
+      <a href="#" className={styles.button} onClick={() => setSelectedButton('Open Source')}>Open Source</a>
+      <a href="#" className={`${styles.button} ${styles.green}`} onClick={() => setSelectedButton('Projects')}>Projects</a>
+      <a href="#" className={`${styles.button} ${styles.purple}`} onClick={() => setSelectedButton('Research')}>Research</a>
+      <a href="#" className={`${styles.button} ${styles.yellow}`} onClick={() => setSelectedButton('NFTs')}>NFTs</a>
+      <a href="#" className={`${styles.button} ${styles.blue}`} onClick={() => setSelectedButton('Culture/Meme')}>Culture/Meme</a>
+      <a href="#" className={styles.button} onClick={() => setSelectedButton('Good Causes')}>Good Causes</a>
+      <a href="#" className={`${styles.button} ${styles.green}`} onClick={() => setSelectedButton('Creators')}>Creators</a>
+      <a href="#" className={`${styles.button} ${styles.purple}`} onClick={() => setSelectedButton('Artists')}>Artists</a>
+      <a href="#" className={`${styles.button} ${styles.yellow}`} onClick={() => setSelectedButton('NSFW')}>NSFW</a>
+      <a href="#" className={`${styles.button} ${styles.blue}`} onClick={() => setSelectedButton('Longtail')}>Longtail</a>
+      <a href="#" className={styles.button} onClick={() => setSelectedButton('Eth Denver')}>Eth Denver</a>
+      <a href="#" className={`${styles.button} ${styles.green}`} onClick={() => setSelectedButton('DeScience')}>DeScience</a>
+      <a href="#" className={`${styles.button} ${styles.purple}`} onClick={() => setSelectedButton('ReFinance')}>ReFinance</a>
+      <a href="#" className={`${styles.button} ${styles.yellow}`} onClick={() => setSelectedButton('Self Growth')}>Self Growth</a>
     </div>
   );
 };
--- apps/app/src/pages/vaults.tsx
+++ apps/app/src/pages/vaults.tsx
@@ -1,35 +1,37 @@
-import { SECONDS_PER_DAY } from '@shared/utilities'
-import { GetStaticProps } from 'next'
-import { getMessages } from 'src/utils'
-import { Layout } from '@components/Layout'
-import { VaultFilters } from '@components/Vault/VaultFilters'
-import { VaultsDisclaimer } from '@components/Vault/VaultsDisclaimer'
-import { VaultsDisplay } from '@components/Vault/VaultsDisplay'
-import { VaultsHeader } from '@components/Vault/VaultsHeader'
+import { useState } from 'react';
+import { SECONDS_PER_DAY } from '@shared/utilities';
+import { GetStaticProps } from 'next';
+import { getMessages } from 'src/utils';
+import { Layout } from '@components/Layout';
+import { VaultFilters } from '@components/Vault/VaultFilters';
+import { VaultsDisclaimer } from '@components/Vault/VaultsDisclaimer';
+import { VaultsDisplay } from '@components/Vault/VaultsDisplay';
+import { VaultsHeader } from '@components/Vault/VaultsHeader';
 import ButtonRow from '@components/Vault/ButtonRow';
 
-
 interface VaultsPageProps {
-  messages: IntlMessages
+  messages: IntlMessages;
 }
 
 export const getStaticProps: GetStaticProps<VaultsPageProps> = async ({ locale }) => {
-  const messages = await getMessages(locale)
+  const messages = await getMessages(locale);
 
   return {
     props: { messages },
-    revalidate: SECONDS_PER_DAY
-  }
-}
+    revalidate: SECONDS_PER_DAY,
+  };
+};
 
 export default function VaultsPage() {
+  const [selectedButton, setSelectedButton] = useState<string | null>(null);
+
   return (
     <Layout className='gap-6 lg:gap-8'>
-      <ButtonRow />
-      <VaultsHeader />
+      <ButtonRow setSelectedButton={setSelectedButton} />
+      <VaultsHeader selectedButton={selectedButton} />
       <VaultFilters />
       <VaultsDisclaimer />
       <VaultsDisplay />
     </Layout>
-  )
+  );
 }
